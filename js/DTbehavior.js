3b6743d7802930ad468350231e2fa3ab32a7e69d
Brett Allen <brettallen777@gmail.com>
2018-04-27 13:03:14 -0400

    Add overlay for each favorites app; need to modify to be unique for each chicklet

---
+++ js/DTbehavior.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.getElementById("BMRCalcNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("BMRCalcNav").style.height = "0%";
+}
