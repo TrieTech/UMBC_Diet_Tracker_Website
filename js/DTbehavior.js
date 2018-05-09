ed1440f96468c397cc16162d0e97fdc3e52bf1d4
Brett Allen <brettallen777@gmail.com>
2018-05-09 01:28:42 -0400

    Load in more data from json file and place into respective text fields in diet tracker app

--- js/DTbehavior.js
+++ js/DTbehavior.js
@@ -19,8 +19,18 @@ function clearBMICalculation(){
 	$('.odometer').html(0);	
 }
 
+function showNutritionStats(){
+	$(".nutrition-facts").fadeIn();
+}
+
+function hideNutritionStats(){
+	$(".nutrition-facts").fadeOut();
+}
+
 /* JQuery to populate the food search box in the diet tracker app*/
 $(document).ready(function(){
+ var values = {};
+ $(".nutrition-facts").fadeOut();
  $.ajaxSetup({ cache: false });
  $('#foodSearch').keyup(function(){
   $('#foodSearchResults').html('');
@@ -31,6 +41,16 @@ $(document).ready(function(){
    $.each(data, function(key, value){
     if (value.Display_Name.search(expression) != -1 || value.Calories.search(expression) != -1)
     {
+     // Create a dictionary of values to grab specific data from json file
+     values[value.Display_Name] = {
+     	"Calories" : value.Calories, "Portion_Amount" : value.Portion_Amount, "Grains" : value.Grains,
+     	"Whole_Grains" : value.Whole_Grains, "Vegetables" : value.Vegetables, "Orange_Vegetables" : value.Orange_Vegetables,
+     	"Drkgreen_Vegetables" : value.Drkgreen_Vegetables, "Starchy_vegetables" : value.Starchy_vegetables, "Other_Vegetables" : value.Other_Vegetables,
+     	"Fruits" : value.Fruits, "Milk" : value.Milk, "Meats" : value.Meats,
+     	"Soy" : value.Soy, "Drybeans_Peas" : value.Drybeans_Peas, "Oils" : value.Oils,
+     	"Solid_Fats" : value.Solid_Fats, "Added_Sugars" : value.Added_Sugars, "Alcohol" : value.Alcohol, "Saturated_Fats" : value.Saturated_Fats
+     };
+
      $('#foodSearchResults').append('<li class="list-group-item link-class">'+value.Display_Name+' | Calories: <span class="text-muted">'+value.Calories+'</span></li>');
     }
    });   
@@ -39,8 +59,29 @@ $(document).ready(function(){
  
  $('#foodSearchResults').on('click', 'li', function() {
   var click_text = $(this).text().split('|');
-  $('#foodSearch').val($.trim(click_text[0]));
+  var displayName = $.trim(click_text[0]);
+  $('#foodSearch').val(displayName);
   $("#foodSearchResults").html('');
-  $("#calor").val($.trim(click_text[1].split(":")[1]));
+
+  // Fill in all the nutrition values
+  $("#calor").val($.trim(values[displayName]["Calories"]));
+  $("#Portion_Amount").val(values[displayName]["Portion_Amount"]);
+  $("#Grains").val(values[displayName]["Grains"]);
+  $("#Whole_Grains").val(values[displayName]["Whole_Grains"]);
+  $("#Vegetables").val(values[displayName]["Vegetables"]);
+  $("#Orange_Vegetables").val(values[displayName]["Orange_Vegetables"]);
+  $("#Drkgreen_Vegetables").val(values[displayName]["Drkgreen_Vegetables"]);
+  $("#Starchy_vegetables").val(values[displayName]["Starchy_vegetables"]);
+  $("#Other_Vegetables").val(values[displayName]["Other_Vegetables"]);
+  $("#Fruits").val(values[displayName]["Fruits"]);
+  $("#Milk").val(values[displayName]["Milk"]);
+  $("#Meats").val(values[displayName]["Meats"]);
+  $("#Soy").val(values[displayName]["Soy"]);
+  $("#Drybeans_Peas").val(values[displayName]["Drybeans_Peas"]);
+  $("#Oils").val(values[displayName]["Oils"]);
+  $("#Solid_Fats").val(values[displayName]["Solid_Fats"]);
+  $("#Added_Sugars").val(values[displayName]["Added_Sugars"]);
+  $("#Alcohol").val(values[displayName]["Alcohol"]);
+  $("#Saturated_Fats").val(values[displayName]["Saturated_Fats"]);
  });
 });
